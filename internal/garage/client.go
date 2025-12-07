dea792e64eac4ce02e476b5c2f5f414aef370741
bmarinov <dev@borismarinov.de>
2025-12-07 16:00:06 +0100

    crd,test: Add CRD definitions and run e2e kubebuilder tests in cluster (#6)
    * Add crd and rbac manifests
    
    * Add ready conditions to print columns
    
    * Add a noop client to test setup
    
    * Add missing rbac kustomization
    
    * Run E2E tests in CI

--- internal/garage/client.go
+++ internal/garage/client.go
@@ -4,23 +4,46 @@ import (
 	"context"
 
 	"github.com/bmarinov/garage-storage-controller/internal/s3"
+	"github.com/google/uuid"
 )
 
 type AdminClient struct {
-	BucketClient
+	*BucketClient
+	*AccessKeyClient
+}
+
+type AccessKeyClient struct {
+}
+
+func (a *AccessKeyClient) Create(ctx context.Context, keyName string) (s3.AccessKey, error) {
+	return s3.AccessKey{
+		ID:     uuid.NewString(),
+		Secret: uuid.NewString(),
+		Name:   keyName,
+	}, nil
+}
+
+func (a *AccessKeyClient) Get(ctx context.Context, id string, search string) (s3.AccessKey, error) {
+	panic("unimplemented")
 }
 
 type BucketClient struct {
 }
 
 func (a *BucketClient) Create(ctx context.Context, globalAlias string) (s3.Bucket, error) {
-	panic("unimplemented")
+	return s3.Bucket{
+		ID:            uuid.NewString(),
+		GlobalAliases: []string{globalAlias},
+	}, nil
 }
 
 func (a *BucketClient) Get(ctx context.Context, globalAlias string) (s3.Bucket, error) {
-	panic("unimplemented")
+	return s3.Bucket{
+		ID:            uuid.NewString(),
+		GlobalAliases: []string{globalAlias},
+	}, nil
 }
 
 func (a *BucketClient) Update(ctx context.Context, id string, quotas s3.Quotas) error {
-	panic("unimplemented")
+	return nil
 }
