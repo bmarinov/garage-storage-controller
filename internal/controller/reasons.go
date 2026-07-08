e056bcf0ca8c2bd90e5082761343881312eeafe8
bmarinov <dev@borismarinov.de>
2026-07-08 22:50:21 +0200

    controller(bucket): Emit events when new Garage buckets are created (#98)

---
+++ internal/controller/reasons.go
@@ -0,0 +1,5 @@
+package controller
+
+const ReasonBucketCreated = "BucketCreated" // type Normal: new bucket in Garage
+
+const ReasonBucketCreateFailed = "BucketCreateFailed" // type Warning: bucket creation in Garage failed
